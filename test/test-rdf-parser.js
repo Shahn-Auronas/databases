6745037939bfe97b244deff6c480d6ab5686d351
Shahn-Auronas <shahn.auronas@gmail.com>
2014-06-07 17:22:23 -0700

    added nodeunit test

--- test/test-rdf-parser.js
+++ test/test-rdf-parser.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const rdfParser = require("/Users/shahnauronas/databases/lib/rdf-parser.js"),
-	  expectedValue = require("./pg132.json");
+	  expectedValue = require("./pg1020.json");
 
 exports.testRDFParser = function (test) {
-	rdfParser(__dirname + "/pg132.rdf", function (err, book) {
+	rdfParser(__dirname + "/pg1020.rdf", function (err, book) {
 		test.expect(2);
 		test.ifError(err);
 		test.deepEqual(book, expectedValue, "book should match expected");
